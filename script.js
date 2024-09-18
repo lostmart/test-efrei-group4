3ff76214b8749db7cdb3f9d7ce5c87a82b4dc8bc
maRtiN p <lostmart@users.noreply.github.com>
2024-09-18 17:13:47 +0200

    with the click

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector("button")
 // rajouter une event listener "click"
 btn.addEventListener("click", (e) => {
 	// console.log(e.target)
-	e.target.innerText = "hello click"
+	e.target.innerText = "hello clicks"
 })
 
 // affiche la variable "btn" dans la console
